c1df9b2aad6c75c43ad4c96f04b866a3cee5dce5
topzyray <topzyray@yahoo.com>
2023-07-13 01:01:35 +0100

    features: first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Navbar, Search } from './components/index'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Search />
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import logo from '../../assets/logo.png';
+
+import './navbar.css'
+
+const Menu = () => (
+  <>
+    <p><a href="#home">HOME</a></p>
+    <p><a href="#wonile">ABOUT US</a></p>
+    <p><a href="#possibility">CONTACT US</a></p>
+  </>
+)
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false)
+ 
+  return (
+    <nav className="onile__navbar">
+      <div className="onile__navbar-links">
+        <div className="onile__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="onile__navbar-links_container">
+          <Menu />
+        </div>
+      </div>
+      <div className="onile__navbar-sign">
+        <p>Sign up</p>
+        <button type="button">Sign in</button>
+      </div>
+      <div className="onile__navbar-menu">
+        {toggleMenu
+          ? <RiCloseLine style={{color: "#fff", background: "#000"}} size={35} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line style={{color: "#fff", background: "#000"}} size={35} onClick={() => setToggleMenu(true)} />
+        }
+        {toggleMenu && (
+          <div className='onile__navbar-menu_container scale-up-center'>
+            <div className='onile__navbar-menu_container-links'>
+              <Menu  />
+              <div className="onile__navbar-menu_container-links-sign">
+                <p>Sign in</p>
+                <button type="button">Sign up</button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar;
